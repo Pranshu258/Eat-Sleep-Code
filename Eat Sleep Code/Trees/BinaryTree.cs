cd99bf273dc95672832d2c842e9f49ddd3bbd163
Pranshu Gupta <pranshug258@outlook.com>
2016-06-05 17:14:51 +0530

    Added code for binary trees, insert, search, min and max

---
+++ Eat Sleep Code/Trees/BinaryTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Eat_Sleep_Code.Trees
+{
+    class BinaryTree: Tree
+    {
+        public Node insert (double key, Node node)
+        {
+            if (node != null)
+            {
+                if (key <= node.key)
+                {
+                    node.left = insert(key, node.left);
+                    node.size = node.size + 1;
+                } 
+                else
+                {
+                    node.right = insert(key, node.right);
+                    node.size = node.size + 1;
+                }
+            }
+            else
+            {
+                node = new Node();
+                node.key = key;
+                node.left = null;
+                node.right = null;
+                node.size = 1;
+            }
+            return node;
+        }
+
+        public Node search(double key, Node node)
+        {
+            if (node != null )
+            {
+                if (key == node.key)
+                {
+                    return node;
+                }
+                else if (key < node.key)
+                {
+                    return search(key, node.left);
+                }
+                else
+                {
+                    return search(key, node.right);
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public void inorderTraversal (Node node)
+        {
+            if (node.left != null)
+            {
+                inorderTraversal(node.left);
+            }
+            Console.WriteLine(string.Format("Value: {0}", node.key));
+            if (node.right != null)
+            {
+                inorderTraversal(node.right);
+            }
+        }
+
+        public Node minimum(Node node)
+        {
+            if (node != null)
+            {
+                if (node.left != null)
+                {
+                    return minimum(node.left);
+                }
+                else
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+
+        public Node maximum(Node node)
+        {
+            if (node != null)
+            {
+                if (node.right != null)
+                {
+                    return maximum(node.right);
+                }
+                else
+                {
+                    return node;
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Eat Sleep Code/Trees/Tree.cs
@@ -0,0 +1,19 @@
+﻿namespace Eat_Sleep_Code.Trees
+{
+
+    public class Node
+    {
+        public double key;
+        public Node left;
+        public Node right;
+        public Node parent;
+        public int size;
+        public int depth;
+    }
+
+    public class Tree
+    {
+        public Node root;
+    }
+
+}
