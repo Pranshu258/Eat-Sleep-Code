d075ec68b133102210f90eab1f3a9318c7a6f7ee
Pranshu Gupta <pranshug258@outlook.com>
2016-11-20 11:52:51 +0530

    added some C# programs

---
+++ C#/Tests/Main.cs
@@ -0,0 +1,29 @@
+﻿using Eat_Sleep_Code.Trees;
+using System;
+
+namespace Eat_Sleep_Code.Test
+{
+    public class MainTest
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTreetest();
+
+            Console.ReadKey();
+        }
+
+        #region Binary Tree Test
+        private static void BinaryTreetest()
+        {
+            var tree = new BinaryTree();
+            var rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                tree.root = tree.insert(i, tree.root);
+            }
+            tree.inorderTraversal(tree.root);
+            Console.WriteLine(tree.search(7, tree.root) != null);
+        }
+        #endregion
+    }
+}
